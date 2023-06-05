9dab9dc05ebfc600f641b1429b288f3c516a9d1e
dvynohradovv <dimarkens@gmail.com>
2023-06-05 23:01:14 +0300

    Change folders

--- university_schedule_generator/forms.py
+++ university_schedule_generator/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class GenerateScheduleForm(forms.Form):
     preset_schedule_csv_file = forms.FileField(
-        label="Select a preset schedule CSV file"
+        label="Select a preset schedule CSV file", required=True
     )
     preset_classrooms_csv_file = forms.FileField(
-        label="Select a preset classrooms CSV file"
+        label="Select a preset classrooms CSV file", required=True
     )
